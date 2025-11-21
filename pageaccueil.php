f684dd620e81ae7b580d2678f21e74aa26f16b60
P-Louison <perrierlouison@gmail.com>
2025-11-21 17:17:24 +0100

    envoi 3

---
+++ pageaccueil.php
@@ -0,0 +1,19 @@
+<?php
+include 'entete.html';
+?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-10">
+            <?php
+                include 'carrousel.php'; 
+            ?>
+        </div>
+        <div class="col-md-2">
+            <?php
+                include 'blocIdentification.html';
+            ?>
+        </div>
+    </div>
+</div>
+
