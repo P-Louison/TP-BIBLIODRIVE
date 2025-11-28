b00a846b0db22677a12670e19f644f841618269b
P-Louison <perrierlouison.prv@gmail.com>
2025-11-28 01:06:42 +0100

    blocIdentification OK

--- blocIdentification.php
+++ blocIdentification.php
@@ -1,7 +1,14 @@
 
     <div class="col-md-2 container-fluid">
-        
-            dezuhfzefzefz
+        <div class="row">
+            <h4> Se Connecter </h4>
+            <br>
+            identifiant : <input type="text" name="mel">
+            <br> 
+            <br>
+            Mot de Passe : <input type="text" name="motdepasse">
+            <br>
+        </div>
     </div>        
 </div>
 
